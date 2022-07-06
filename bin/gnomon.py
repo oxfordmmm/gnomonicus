e7df05dd8abfe07c229dc982b695f357712bbc6c
JeremyWesthead <jeremywesthead3@gmail.com>
2022-07-06 16:45:18 +0100

    Added effects generation. Requires testing but should function almost the same as sp3predict now

--- bin/gnomon.py
+++ bin/gnomon.py
@@ -2,7 +2,7 @@
     with a reference genome and a resistance catalogue, and utilises gumpy and 
     piezo to produce variants, mutations and an antibiogram.
 
-Based on sp3predict, but cleaned, resiliant and tested.
+Based on sp3predict
 '''
 import argparse
 import logging
@@ -18,6 +18,12 @@ class MissingFieldException(Exception):
     '''Custom exception for when required fields are missing from a given table
     '''
     def __init__(self, field: str, table: str) -> None:
+        '''Raise this exception
+
+        Args:
+            field (str): Field name missing
+            table (str): Table which the field is missing from
+        '''
         self.message = f"Field: {field} is not in the {table} table!"
         super().__init__(self.message)
 
@@ -26,6 +32,19 @@ class NoVariantsException(Exception):
     '''
     def __init__(self):
         super().__init__("No variants were detected!")
+
+class InvalidMutationException(Exception):
+    '''Custom exception raised when an invalid mutation is detected
+    '''
+    def __init__(self, gene: str, mutation: str):
+        '''Raise this exception
+
+        Args:
+            gene (str): Name of the gene
+            mutation (str): The invalid mutation
+        '''
+        self.message = f"{gene}@{mutation} is not a valid mutation!"
+        super().__init__(self.message)
 
 
 def loadGenome(path: str) -> gumpy.Genome:
@@ -98,7 +117,7 @@ def populateVariants(vcfStem: str, outputDir: str, diff: gumpy.GenomeDifference)
         #Save CSV
         variants.to_csv(os.path.join(outputDir, 'variants.csv'), header=True)
 
-def populateMutations(vcfStem: str, outputDir: str, diff: gumpy.GenomeDifference, reference: gumpy.Genome, sample: gumpy.Genome, resistanceCatalogue: piezo.ResistanceCatalogue) -> pd.DataFrame:
+def populateMutations(vcfStem: str, outputDir: str, diff: gumpy.GenomeDifference, reference: gumpy.Genome, sample: gumpy.Genome, resistanceCatalogue: piezo.ResistanceCatalogue) -> (pd.DataFrame, dict):
     '''Popuate and save the mutations DataFrame as a CSV, then return it for use in predictions
 
     Args:
@@ -114,20 +133,30 @@ def populateMutations(vcfStem: str, outputDir: str, diff: gumpy.GenomeDifference
 
     Returns:
         pd.DataFrame: The mutations DataFrame
+        dict: Dictionary mapping gene name --> reference gumpy.Gene object
     '''
-    #Find the resistance associated genes which also have mutations in this sample
-    #This considerably cuts back on the number of genes which have to be explored
-    mask = np.isin(reference.stacked_nucleotide_index, diff.nucleotide_index)
-    genesWithMutations = np.unique(reference.stacked_gene_name[mask])
-    detectedResistanceGenes = set(resistanceCatalogue.catalogue.genes).intersection(set(genesWithMutations))
+    if resistanceCatalogue:
+        #Find the resistance associated genes which also have mutations in this sample
+        #This considerably cuts back on the number of genes which have to be explored
+        mask = np.isin(reference.stacked_nucleotide_index, diff.nucleotide_index)
+        genesWithMutations = np.unique(reference.stacked_gene_name[mask])
+        detectedResistanceGenes = set(resistanceCatalogue.catalogue.genes).intersection(set(genesWithMutations))
+    else:
+        #No catalogue, so just stick to genes in the sample
+        detectedResistanceGenes = sample.genes
 
     #Iter resistance genes with variation to produce gene level mutations - concating into a single dataframe
     mutations = None
+    referenceGenes = {}
     #TODO: Might be worth optimising this to avoid concating dfs as they are expensive to work with?
     for gene in detectedResistanceGenes:
         if gene:
             logging.debug(f"Found a resistance gene with mutation: {gene}")
-            diff = reference.build_gene(gene) - sample.build_gene(gene)
+            #Save the reference genes for use later in effects.csv
+            refGene = reference.build_gene(gene)
+            referenceGenes[gene] = refGene
+            #Get gene difference
+            diff = refGene - sample.build_gene(gene)
             #Pull the data out of the gumpy object
             geneMutations = pd.DataFrame({
                 'MUTATION': diff.mutations,
@@ -190,7 +219,7 @@ def populateMutations(vcfStem: str, outputDir: str, diff: gumpy.GenomeDifference
 
         #Remove index to return
         mutations.reset_index(inplace=True)
-    return mutations
+    return mutations, referenceGenes
     
 
 def assignMutationBools(row: pd.Series) -> pd.Series:
@@ -229,8 +258,80 @@ def countNucleotideChanges(row: pd.Series) -> int:
     else:
         return 0
 
-def populateEffects(vcfStem: str, outputDir: str) -> None:
-    pass
+def populateEffects(sample: gumpy.Genome, outputDir: str, resistanceCatalogue: piezo.ResistanceCatalogue, mutations: pd.DataFrame, referenceGenes: dict) -> dict:
+    '''Populate and save the effects DataFrame as a CSV
+
+    Args:
+        sample (gumpy.Genome): The Genome object of the sample
+        outputDir (str): Path to the directory to save the CSV
+        resistanceCatalogue (piezo.ResistanceCatalogue): Resistance catalogue for predictions
+        mutations (pd.DataFrame): Mutations dataframe
+        referenceGenes (dict): Dictionary mapping gene name --> reference gumpy.Gene objects
+
+    Raises:
+        InvalidMutationException: Raised if an invalid mutation is detected
+
+    Returns:
+        dict: A metadata dictionary mapping drugs to their predictions
+    '''
+    #Assume wildtype behaviour unless otherwise specified
+    phenotype = {drug: 'S' for drug in resistanceCatalogue.catalogue.drugs}
+
+    effects = {}
+    effectsCounter = 0
+
+    #Default prediction values are RFUS but use piezo catalogue's values if existing
+    values = resistanceCatalogue.catalogue.values
+    if not values:
+        values = list("RFUS")
+
+    for (gene, mutation) in zip(mutations['GENE'], mutations['MUTATION']):
+        #Ensure its a valid mutation
+        if not referenceGenes[gene].valid_variant(mutation):
+            logging.error(f"Not a valid mutation {gene}@{mutation}")
+            raise InvalidMutationException(gene, mutation)
+        
+        #Get the prediction
+        prediction = resistanceCatalogue.predict(gene+'@'+mutation)
+
+        #If the prediction is interesting, iter through drugs to find predictions
+        if prediction != 'S':
+            for drug in prediction.keys():
+                #Check for empty strings
+                if not drug:
+                    continue
+
+                #Prioritise values based on order within the values list
+                if values.index(prediction[drug]) < values.index(phenotype[drug]):
+                    #The prediction is closer to the start of the values list, so should take priority
+                    phenotype[drug] = prediction[drug]
+
+                #Add to the dict
+                effects[effectsCounter] = [sample.name, gene, mutation, resistanceCatalogue.catalogue.name, drug, prediction[drug]]
+        else:
+            #Add the no effect to the dict
+            effects[effectsCounter] = [sample.name, gene, mutation, resistanceCatalogue.catalogue.name, "UNK", "S"]
+        #Increment counter
+        effectsCounter += 1
+    
+    #Build the DataFrame
+    effects = pandas.DataFrame.from_dict(effects, 
+                                        orient="index", 
+                                        columns=["UNIQUEID", "GENE", "MUTATION", 
+                                            "CATALOGUE_NAME", "DRUG", "PREDICTION"]
+                                        )
+    
+    #Set the index
+    effects.set_index(["UNIQUEID", "DRUG", "GENE", "MUTATION", "CATALOGUE_NAME"], inplace=True)
+    
+    #Save as CSV
+    effects.to_csv(os.path.join(outputDir, 'effects.csv'))
+
+    #Return  the metadata dict to log later
+    return {"WGS_PREDICTION_"+drug: phenotype[drug] for drug in resistanceCatalogue.catalogue.drugs}
+
+                
+
     
 
 
@@ -246,7 +347,7 @@ if __name__ == "__main__":
     parser.add_argument("--output_dir", action="store_true", required=False, default=".", help="Directory to save output files to. Defaults to wherever the script is run from.")
     options = parser.parse_args()
 
-    #Make the directory if it doesn't already exist
+    #Make the output directory if it doesn't already exist
     os.makedirs(options.output_dir, exist_ok=True)
 
     #Logging setup
@@ -267,8 +368,12 @@ if __name__ == "__main__":
     logging.debug("Got VCF stem")
 
     #Get resistance catalogue
-    resistanceCatalogue=piezo.ResistanceCatalogue(options.catalogue_file, prediction_subset_only=True)
-    logging.debug("Loaded resistance catalogue")
+    if options.catalogue_file:
+        resistanceCatalogue = piezo.ResistanceCatalogue(options.catalogue_file, prediction_subset_only=True)
+        logging.debug("Loaded resistance catalogue")
+    else:
+        resistanceCatalogue = None
+        logging.info("No resistance catalogue provided, producing variants and mutations only")
 
     #Get the GenomeDifference for extracting genome level mutations
     diff = reference - sample
@@ -283,10 +388,33 @@ if __name__ == "__main__":
     populateVariants(vcfStem, options.output_dir, diff)
     logging.debug("Populated and saved variants.csv")
 
-    mutations = populateMutations(vcfStem, options.output_dir, diff, 
+    mutations, referenceGenes = populateMutations(vcfStem, options.output_dir, diff, 
                         reference, sample, resistanceCatalogue)
-    logging.debug("Populated and saved mutatons.csv")
+    if not mutations:
+        logging.info("No mutations found - probably due to exclusively inter-gene variation or no variation.\n\t\t\t\t\t\t\t No effects.csv written")
+    else:
+        logging.debug("Populated and saved mutatons.csv")
 
     #Get the effects of the mutations
-    populateEffects(vcfStem, options.output_dir, mutations)
-    logging.debug("Populated and saved effects.csv")
+    if resistanceCatalogue and mutations:
+        metadata = populateEffects(sample, options.output_dir, resistanceCatalogue, mutations, referenceGenes)
+        logging.debug("Populated and saved effects.csv")
+    else:
+        metadata = {}
+        logging.info("Skipped effects.csv due to lack of resistance catalogue or mutations")
+
+    #Add data to the log
+    logging.info("********** Successfully completed **********")
+    
+    logging.info(f"VCF file: {options.vcf_file}")
+    logging.info(f"Reference genome file: {options.genome_object}")
+
+    if resistanceCatalogue:
+        logging.info(f"Catalogue reference genome: {resistanceCatalogue.catalogue.genbank_reference}")
+        logging.info(f"Catalogue name: {resistanceCatalogue.catalogue.name}")
+        logging.info(f"Catalogue version: {resistanceCatalogue.catalogue.version}")
+        logging.info(f"Catalogue grammar: {resistanceCatalogue.catalogue.grammar}")
+        logging.info(f"Catalogue values: {resistanceCatalogue.catalogue.values}")
+        logging.info(f"Catalogue path: {options.catalogue_file}")
+    for drug in sorted(metadata.keys()):
+        logging.info(f"{drug} {metadata[drug]}")
