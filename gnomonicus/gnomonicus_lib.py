5bd8b9b7e5e0f513c8c6186d07550f3570f344f8
JeremyWesthead <jeremywesthead3@gmail.com>
2026-08-19 12:19:44 +0100

    chore: appease pc

--- gnomonicus/gnomonicus_lib.py
+++ gnomonicus/gnomonicus_lib.py
@@ -986,7 +986,10 @@ def populateEffects(
                     evidence: dict = {}
                 else:
                     pred, evidence = drug_pred
-                if pred == "S" and evidence.get("reporting_rule", "") in ["minor allele, no matching rule found", "null call, no matching rule found"]:
+                if pred == "S" and evidence.get("reporting_rule", "") in [
+                    "minor allele, no matching rule found",
+                    "null call, no matching rule found",
+                ]:
                     # This is a minor allele or null call with no matching rule, so ignore it
                     # These aren't real predictions, and add nothing to the phenotype, so we can skip them
                     continue
